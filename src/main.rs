adf470bc9aff4c7b8b8eb86c23e36eebefbabfa2
technosoul <rockdrigo.mtz@gmail.com>
2018-11-01 19:14:19 -0600

    adding our first pattern maching and enum Ordering ^_^

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1,101);
 
     println!("The secret number is: {}", secret_number);
-
     println!("Please input your guess.");
 
     let mut guess = String::new();
@@ -19,4 +18,10 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("To small!"),
+        Ordering::Greater => println!("To big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
